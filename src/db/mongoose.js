5a60cfa43fb007e577a50a5de08cfc7771ce495b
Mattia Piano <hypnos.design@gmail.com>
2019-11-06 14:57:52 +0100

    prima configurazione

---
+++ src/db/mongoose.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+ const User = mongoose.model('User',{
+   name: {
+     type: String
+   },
+   password: {
+     type: Number
+   }
+ });
+
+const Prodotti = mongoose.model('Prodotti',{
+  codice: {
+    type: Number
+  },
+  codiceFornitore: {
+    type: Number
+  },
+  descrizione: {
+    type: String
+  },
+  prezzo: {
+    type: Number
+  },
+  qnt: {
+    type: Number
+  }
+
+});
+
+ const me = new User({
+   name: 'Mattia',
+   password: 8080
+ });
+
+ me.save().then(result => console.log(result)).catch( err => console.log(err));
